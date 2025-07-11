73a1298973c85a8e31b69b1424fe48cdda4488ca
cimke <tcimermonas@gmail.com>
2025-07-11 21:03:54 +0300

    chore(test): 🔧 🚨 remove default test file, add player tests / update package

---
+++ src/entrypoints/player.resolver.ts
@@ -0,0 +1,58 @@
+import { Resolver, Query, Mutation, Args, ID, ObjectType, Field } from '@nestjs/graphql';
+import { Inject } from '@nestjs/common';
+import { PlayerServiceImpl } from '../domain/services/manager';
+
+@ObjectType()
+export class Player {
+  @Field(() => ID)
+  id!: string;
+
+  @Field()
+  name!: string;
+}
+
+@Resolver(() => Player)
+export class PlayerResolver {
+  constructor(
+    @Inject(PlayerServiceImpl)
+    private readonly playerService: PlayerServiceImpl,
+  ) {}
+
+  private handleError(error: unknown): never {
+    if (error instanceof Error) {
+      if (process.env.NODE_ENV === 'development') {
+        throw new Error(`${error.message}\n${error.stack}`);
+      }
+      throw new Error(error.message);
+    }
+    throw new Error('Unknown error');
+  }
+
+  @Query(() => Player, { name: 'player' })
+  async getPlayer(@Args('id', { type: () => ID }) id: string): Promise<Player> {
+    try {
+      return await this.playerService.getPlayer(id);
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  @Mutation(() => Player)
+  async createPlayer(@Args('name') name: string): Promise<Player> {
+    try {
+      return await this.playerService.createPlayer(name);
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async deletePlayer(@Args('id', { type: () => ID }) id: string): Promise<boolean> {
+    try {
+      await this.playerService.deletePlayer(id);
+      return true;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+} 
--- test/jest.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Tesing JEST framework', () => {
-  test('It works', () => {
-    expect(1).toEqual(1);
-  });
-});
